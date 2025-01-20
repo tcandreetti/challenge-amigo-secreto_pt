47b21d1d0e68e5f9a631d776493df32ffdcc435b
Thais Andreetti <thaiscandreetti@gmail.com>
2025-01-20 17:25:15 +0100

    Adicionando as primeiras funções

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigosSorteados = [];
+let nome = ''
+const listaAmigos = document.getElementById('listaAmigos');
+const nomeInput = document.getElementById('amigo');
+
+nomeInput.addEventListener('keydown', function(event) {
+    if (event.keyCode === 13) {
+        adicionarAmigo();
+    }
+});
+
+function mostrarLista(campo){
+    const novoItem = document.createElement('li');
+    novoItem.classList.add('name-item');
+    novoItem.textContent = campo;
+    listaAmigos.appendChild(novoItem);
+}
+
+function sortearAmigoSecreto(listaDeAmigos) {
+
+
+
+}
+
+function contemNome(listaAmigos, nome) {
+    return listaAmigos.includes(nome);
+}
+
+function adicionarAmigo(){   
+    let amigo = nomeInput.value;
+    if (amigo == ''){
+        alert('O campo não pode estar vazio');
+        return;
+    }
+    else{
+        if(contemNome(listaDeAmigosSorteados,amigo)){
+            alert('Este amigo já foi inserido');
+            nomeInput.value = '';
+            }
+        else {
+            listaDeAmigosSorteados.push(amigo);
+            mostrarLista(amigo);
+            nomeInput.value = '';
+            }
+           
+        }
+        
+    }
+
+
+let resposta = prompt("Você irá participar deste amigo secreto? \n Responda SIM ou NÃO")
+resposta = resposta.toUpperCase();
+if(resposta =='SIM'){
+    let campo = prompt("Digite o seu nome para começar:");
+    listaDeAmigosSorteados.push(campo);
+    mostrarLista(campo);
+}
+
