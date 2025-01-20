c902869ff82502c600f64ac37b6bd20ce7e47e62
Thais Andreetti <thaiscandreetti@gmail.com>
2025-01-20 19:24:26 +0100

    Inserção da Função de Reiniciar e Remoção do seu próprio nome

--- app.js
+++ app.js
@@ -1,32 +1,58 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let listaDeAmigosSorteados = [];
-let nome = ''
 const listaAmigos = document.getElementById('listaAmigos');
 const nomeInput = document.getElementById('amigo');
+const resultado = document.getElementById('resultado');
+const botaoReiniciar = document.getElementById('reiniciar');
+const botaoSortear = document.getElementById('sortear');
+const botaoAdicionar = document.getElementById('adicionar');
 
+let listaDeAmigosSorteados = [];
+
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag)
+        campo.innerHTML = texto;}
+
+// Evento para adicionar amigo com a tecla enter
 nomeInput.addEventListener('keydown', function(event) {
     if (event.keyCode === 13) {
         adicionarAmigo();
+        nomeInput.value = '';
+        exibirTextoNaTela('h2',"Digite o nome dos seus amigos: ");
     }
 });
 
+//Mostra a Lista de Amigos inseridos
 function mostrarLista(campo){
     const novoItem = document.createElement('li');
     novoItem.classList.add('name-item');
     novoItem.textContent = campo;
-    listaAmigos.appendChild(novoItem);
+    listaAmigos.appendChild(novoItem);;
+    exibirTextoNaTela('h2',"Digite o nome dos seus amigos: ")
 }
 
-function sortearAmigoSecreto(listaDeAmigos) {
-
-
-
+function apagarLista(){
+    listaAmigos.innerHTML = '';
 }
 
+function sortearAmigo() {
+    //vamos evitar que você e seu amigo sorteado sejam a mesma pessoa
+    listaDeAmigosSorteados.splice(0,1);
+    // sorteio do amigo
+    nomeSorteado = listaDeAmigosSorteados[Math.floor(Math.random() * listaDeAmigosSorteados.length)];
+    apagarLista();
+    resultado.textContent = `O amigo sorteado foi: ${nomeSorteado}`;
+    botaoSortear.disabled = true;
+    botaoAdicionar.disabled = true;
+    return nomeSorteado;
+}
+
+//Verifica se um nome já foi inserido previamente
 function contemNome(listaAmigos, nome) {
     return listaAmigos.includes(nome);
 }
 
+//Adiciona um amigo na lista
 function adicionarAmigo(){   
     let amigo = nomeInput.value;
     if (amigo == ''){
@@ -47,13 +73,13 @@ function adicionarAmigo(){
         }
         
     }
-
-
-let resposta = prompt("Você irá participar deste amigo secreto? \n Responda SIM ou NÃO")
-resposta = resposta.toUpperCase();
-if(resposta =='SIM'){
-    let campo = prompt("Digite o seu nome para começar:");
-    listaDeAmigosSorteados.push(campo);
-    mostrarLista(campo);
+function reiniciar(){
+    resultado.innerHTML=''
+    nomeInput.value= '';
+    botaoAdicionar.disabled = false;
+    botaoSortear.disabled = false;
+    exibirTextoNaTela('h2',"Digite o seu nome para começar: ")
+    listaDeAmigosSorteados = [];
 }
 
+exibirTextoNaTela('h2',"Digite o seu nome para começar: ")
